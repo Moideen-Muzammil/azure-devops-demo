a182d4842eccecfe01ed9d1743bf8074067893d6
muzammil <moideenmuzammil15@gmail.com>
2025-12-18 20:26:18 +0530

    Add start script for Azure Node deployment

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Serve static files from current directory
+app.use(express.static(__dirname));
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
+
